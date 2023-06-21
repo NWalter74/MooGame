319461e4832b6849a6fe454267efcfbf8dd0db9f
NWalter74 <nicole@die-walter-familie.de>
2023-06-21 16:27:56 +0200

    - Huvudmetoden är uppdelad i mindre, fokuserade funktioner för att förbättra läsbarheten och underhållbarheten. - Den relaterade koden för att skapa målet, kontrollera användarens gissning och spara spelardata flyttas till separata funktioner. - Inläsningen av spelardata från en fil och visning av topplistan över spelare flyttas till separata funktioner. - Using statement används för korrekt hantering av resurser (t.ex. StreamWriter och StreamReader). - Hjälpfunktioner introduceras för att kontrollera om gissningen är korrekt och för att avgöra om programmet ska avslutas. - Gemensam funktionalitet är inkapslad i lämpliga funktioner.

--- MooGame/Program.cs
+++ MooGame/Program.cs
@@ -25,7 +25,7 @@ internal class Program
             int nGuess = 1;
             string bbcc = CheckBC(goal, guess);
             Console.WriteLine(bbcc + "\n");
-            while (bbcc != "BBBB,")
+            while (!IsCorrectGuess(bbcc))
             {
                 nGuess++;
                 guess = Console.ReadLine();
@@ -37,7 +37,7 @@ internal class Program
             ShowTopList();
             Console.WriteLine("Correct, it took " + nGuess + " guesses\nContinue?");
             string answer = Console.ReadLine();
-            if (answer != null && answer != "" && answer.Substring(0, 1) == "n")
+            if (ShouldExit(answer))
             {
                 playOn = false;
             }
@@ -88,41 +88,60 @@ internal class Program
 
     static void SavePlayerData(string name, int nGuess)
     {
-        StreamWriter output = new StreamWriter("result.txt", append: true);
-        output.WriteLine(name + "#&#" + nGuess);
-        output.Close();
+        using (StreamWriter output = new StreamWriter("result.txt", append: true))
+        {
+            output.WriteLine(name + "#&#" + nGuess);
+        }
     }
 
     static void ShowTopList()
     {
-        StreamReader input = new StreamReader("result.txt");
-        List<PlayerData> results = new List<PlayerData>();
-        string line;
-        while ((line = input.ReadLine()) != null)
-        {
-            string[] nameAndScore = line.Split(new string[] { "#&#" }, StringSplitOptions.None);
-            string name = nameAndScore[0];
-            int guesses = Convert.ToInt32(nameAndScore[1]);
-            PlayerData pd = new PlayerData(name, guesses);
-            int pos = results.IndexOf(pd);
-            if (pos < 0)
-            {
-                results.Add(pd);
-            }
-            else
-            {
-                results[pos].Update(guesses);
-            }
+        List<PlayerData> results = LoadPlayerData();
 
-
-        }
         results.Sort((p1, p2) => p1.GetAverage().CompareTo(p2.GetAverage()));
         Console.WriteLine("Player   games average");
         foreach (PlayerData p in results)
         {
             Console.WriteLine(string.Format("{0,-9}{1,5:D}{2,9:F2}", p.Name, p.NGames, p.GetAverage()));
         }
-        input.Close();
+    }
+
+    static List<PlayerData> LoadPlayerData()
+    {
+        List<PlayerData> results = new List<PlayerData>();
+
+        using (StreamReader input = new StreamReader("result.txt"))
+        {
+            string line;
+            while ((line = input.ReadLine()) != null)
+            {
+                string[] nameAndScore = line.Split(new string[] { "#&#" }, StringSplitOptions.None);
+                string name = nameAndScore[0];
+                int guesses = Convert.ToInt32(nameAndScore[1]);
+                PlayerData pd = new PlayerData(name, guesses);
+                int pos = results.IndexOf(pd);
+                if (pos < 0)
+                {
+                    results.Add(pd);
+                }
+                else
+                {
+                    results[pos].Update(guesses);
+                }
+            }
+        }
+
+        return results;
+    }
+
+    static bool IsCorrectGuess(string bbcc)
+    {
+        return bbcc == "BBBB,";
+    }
+
+    static bool ShouldExit(string answer)
+    {
+        return !string.IsNullOrEmpty(answer) && answer.Substring(0, 1) == "n";
     }
 }
 
