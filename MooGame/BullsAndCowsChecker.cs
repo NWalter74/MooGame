8371a4c05489865487a44a8584b580f7aa2194a3
NWalter74 <nicole@die-walter-familie.de>
2023-07-03 22:26:38 +0200

    - Jag använde Singleton-designmönstret för klassen StatisticsCollector för att säkerställa att endast en instans av klassen finns i hela applikationen. Detta kommer att ge en central åtkomstpunkt för att hantera och hämta spelarstatistik. - Jag implementerade Factory designmönster för att skapa instanser av gränssnitten IUserInterface, IGame och IStatisticsCollector. Detta kommer att möjliggöra flexibilitet i att skapa olika typer av användargränssnitt, spel och statistiksamlare i framtiden. - Jag implementerade Strategy designmönster för spellogiken. Jag skapade en separat klass för att skapa mål, kontrollera BC (tjurar och kor) och fastställa korrekta gissningar. Detta kommer att skilja spellogiken från MooGame-klassen och göra den mer modulär och testbar.

---
+++ MooGame/BullsAndCowsChecker.cs
@@ -0,0 +1,33 @@
+﻿namespace MooGame;
+
+public class BullsAndCowsChecker
+{
+    public string CheckBC(string goal, string guess)
+    {
+        int cows = 0, bulls = 0;
+        guess += "    ";
+        for (int i=0; i<4; i++)
+        {
+            for(int j=0; j<4; j++)
+            {
+                if (goal[i] == guess[j])
+                {
+                    if(i == j)
+                    {
+                        bulls++;
+                    }
+                    else
+                    {
+                        cows++;
+                    }
+                }
+            }
+        }
+        return "BBBB".Substring(0, bulls) + "," + "CCCC".Substring(0, cows);
+    }
+
+    public bool IsCorrectGuess(string bbcc)
+    {
+        return bbcc == "BBBB,";
+    }
+}
---
+++ MooGame/GoalGenerator.cs
@@ -0,0 +1,28 @@
+﻿namespace MooGame;
+
+public class GoalGenerator
+{
+    private Random randomGenerator;
+
+    public GoalGenerator()
+    {
+        randomGenerator = new Random();
+    }
+
+    public string GenerateGoal()
+    {
+        string goal = "";
+        for (int i=0; i<4; i++)
+        {
+            int random = randomGenerator.Next(10);
+            string randomDigit = "" + random;
+            while (goal.Contains(randomDigit))
+            {
+                random= randomGenerator.Next(10);
+                randomDigit = "" + random;
+            }
+            goal = goal + randomDigit;
+        }
+        return goal;
+    }
+}
