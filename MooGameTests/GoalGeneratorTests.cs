1c1a07d3510cbf2f42f9847c74ed3af39afb8de3
NWalter74 <nicole@die-walter-familie.de>
2023-07-06 23:06:48 +0200

    UnitTests för: - BullsAndCowsCheckerTests.CheckBC_ShouldReturnExpectedResult - BullsAndCowsCheckerTests.IsCorrectGuess_ShouldReturnTrue_WhenBullsAndCowsAreCorrect: - GoalGeneratorTests.GenerateGoal_ReturnsFourDigitNumber: - PlayerDataTests.GetAverage_ReturnsCorrectAverage:

---
+++ MooGameTests/GoalGeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿namespace MooGameTests;
+
+[TestClass]
+public class GoalGeneratorTests
+{
+    [TestMethod]
+    public void GenerateGoal_ReturnsFourDigitNumber()
+    {
+        // Arrange
+        GoalGenerator goalGenerator = new GoalGenerator();
+
+        // Act
+        string goal = goalGenerator.GenerateGoal();
+
+        // Assert
+        Assert.AreEqual(4, goal.Length);
+        foreach (char c in goal)
+        {
+            Assert.IsTrue(char.IsDigit(c));
+        }
+    }
+}
---
+++ MooGameTests/PlayerDataTests.cs
@@ -0,0 +1,18 @@
+﻿namespace MooGameTests;
+
+[TestClass]
+public class PlayerDataTests
+{
+    [TestMethod]
+    public void GetAverage_ReturnsCorrectAverage()
+    {
+        // Arrange
+        PlayerData playerData = new PlayerData("John", 10);
+
+        // Act
+        double average = playerData.GetAverage();
+
+        // Assert
+        Assert.AreEqual(10.0, average);
+    }
+}
