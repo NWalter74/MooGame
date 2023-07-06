311e31edae061d0d5ead8e8f38f559c6d899c1e9
NWalter74 <nicole@die-walter-familie.de>
2023-07-06 23:08:18 +0200

    Rensat bort utkommenterat kod

--- MooGameTests/BullsAndCowsCheckerTests.cs
+++ MooGameTests/BullsAndCowsCheckerTests.cs
@@ -1,25 +1,23 @@
-using Moq;
-
 namespace MooGameTests;
 
 [TestClass]
 public class BullsAndCowsCheckerTests
 {
-    //[TestMethod]
-    //public void CheckBC_ShouldReturnExpectedResult()
-    //{
-    //    // Arrange
-    //    var bullsAndCowsChecker = new BullsAndCowsChecker();
-    //    string goal = "1234";
-    //    string guess = "1243";
-    //    string expected = "BB,CC";
+    [TestMethod]
+    public void CheckBC_ShouldReturnExpectedResult()
+    {
+        // Arrange
+        var bullsAndCowsChecker = new BullsAndCowsChecker();
+        string goal = "1234";
+        string guess = "1243";
+        string expected = "BB,CC";
 
-    //    // Act
-    //    string result = bullsAndCowsChecker.CheckBC(goal, guess);
+        // Act
+        string result = bullsAndCowsChecker.CheckBC(goal, guess);
 
-    //    // Assert
-    //    Assert.AreEqual(expected, result);
-    //}
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
 
     [TestMethod]
     public void IsCorrectGuess_ShouldReturnTrue_WhenBullsAndCowsAreCorrect()
@@ -34,79 +32,4 @@ public class BullsAndCowsCheckerTests
         // Assert
         Assert.IsTrue(result);
     }
-
-    [TestMethod]
-    public void CheckBC_ShouldReturnExpectedResult()
-    {
-        // Arrange
-        var bullsAndCowsChecker = new BullsAndCowsChecker();
-        string goal = "1234";
-        string guess = "1243";
-        string expected = "BBCC";
-
-        // Act
-        string result = bullsAndCowsChecker.CheckBC(goal, guess);
-
-        // Assert
-        Assert.AreEqual(expected, result);
-    }
-
-    [TestMethod]
-    public void ConsoleUserInterface_GetUserName_ShouldReturnExpectedValue()
-    {
-        // Arrange
-        var userInterfaceMock = new Mock<IUserInterface>();
-        userInterfaceMock.Setup(ui => ui.GetUserName()).Returns("John");
-
-        var inventoryController = new InventoryController(userInterfaceMock.Object, null, null);
-
-        // Act
-        string userName = inventoryController.GetUserName();
-
-        // Assert
-        Assert.AreEqual("John", userName);
-    }
-
-    //[TestMethod]
-    //public void CheckBC_ShouldReturnCorrectBullsAndCowsCount()
-    //{
-    //    // Arrange
-    //    string goal = "1234";
-    //    string guess = "1423";
-    //    BullsAndCowsChecker checker = new BullsAndCowsChecker();
-
-    //    // Act
-    //    string result = checker.CheckBC(goal, guess);
-
-    //    // Assert
-    //    Assert.AreEqual("B,CCC", result);
-    //}
-
-    //[TestMethod]
-    //public void IsCorrectGuess_ShouldReturnTrueForCorrectGuess()
-    //{
-    //    // Arrange
-    //    string bbcc = "BBBB,";
-    //    BullsAndCowsChecker checker = new BullsAndCowsChecker();
-
-    //    // Act
-    //    bool result = checker.IsCorrectGuess(bbcc);
-
-    //    // Assert
-    //    Assert.IsTrue(result);
-    //}
-
-    //[TestMethod]
-    //public void IsCorrectGuess_ShouldReturnFalseForIncorrectGuess()
-    //{
-    //    // Arrange
-    //    string bbcc = "BBCC";
-    //    BullsAndCowsChecker checker = new BullsAndCowsChecker();
-
-    //    // Act
-    //    bool result = checker.IsCorrectGuess(bbcc);
-
-    //    // Assert
-    //    Assert.IsFalse(result);
-    //}
 }
