73ab34038b9d2dc568b14d3c129479d4c75045ef
NWalter74 <nicole@die-walter-familie.de>
2023-07-25 21:40:23 +0200

    -BullsAndCowsCheckerTests.CheckBC_ShouldReturnExpectedResult -BullsAndCowsCheckerTests.IsCorrectGuess_ShouldReturnTrue_WhenBullsAndCowsAreCorrect - GoalGeneratorTests.GenerateGoal_ReturnsFourDigitNumber - PlayerDataTests.GetAverage_ReturnsCorrectAverage

---
+++ MooGameTest/PlayerDataTests.cs
@@ -0,0 +1,21 @@
+﻿using MooGame;
+
+namespace MooGameTest
+{
+    [TestClass]
+    public class PlayerDataTests
+    {
+        [TestMethod]
+        public void GetAverage_ReturnsCorrectAverage()
+        {
+            // Arrange
+            PlayerData playerData = new PlayerData("John", 10);
+
+            // Act
+            double average = playerData.GetAverage();
+
+            // Assert
+            Assert.AreEqual(10.0, average);
+        }
+    }
+}
